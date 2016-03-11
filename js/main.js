94a23d7b11092b6ca6c75575a826731d76e2d1ce
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-11 16:11:23 -0500

    added all variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-var $header = $(header);
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $mainp  = $('main >p:firstchild');
+var $h2 = $('h2');
+var $mainlist = $('main list');
+var $img = $('img');
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+
+// addClass\('')
+// removeClass\('')
+// show\
