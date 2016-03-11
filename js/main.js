de78a24238870cb6004c3fa3607a41078641a736
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-11 16:37:12 -0500

    added the changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
-var $mainp  = $('main >p:firstchild');
+var $mainP  = $('main p:first-child');
 var $h2 = $('h2');
-var $mainlist = $('main list');
+var $mainList = $('main list');
 var $img = $('img');
 var $dl = $('dl');
 var $dt = $('dt');
+var $lastList = $('ul li:last-child');
+var $firstList = $('ul li:first-child');
 
 $header.addClass('masthead');
 
 $h1.removeClass('big-heading');
 $h1.addClass('logo');
 
-// addClass\('')
-// removeClass\('')
+$main.addClass('wrapper');
+
+$mainP.addClass('intro');
+
+$h2.addClass('slug-head');
+
+$mainList.addClass('slug-list');
+
+$img.addClass('slug-img');
+
+$firstList.addClass('slug-list-first');
+$lastList.addClass('slug-list-last');
+
+$dl.addClass('classification');
+
+$dt.removeClass('big-label');
+
+$img.addClass('slug-img');
+
+$lastList.show();
+
 // show\
